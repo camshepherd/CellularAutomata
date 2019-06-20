38d0d94ff22d612e4a41667ae6ff6da69258d535
shepherd c.m. (cms1u18) <cms1u18@soton.ac.uk>
2019-06-20 10:04:19 +0100

    Added Core SimulatorCPU Tests

--- CellularAutomataTests/SimulatorCPUTests.cpp
+++ CellularAutomataTests/SimulatorCPUTests.cpp
@@ -16,5 +16,37 @@ namespace SimulatorTesting {
 			SimulatorCPU sim{6,4, con, strips};
 			Assert::IsTrue(true);
 		}
+
+		TEST_METHOD(CanStepForward) {
+			RulesConway con = RulesConway();
+			SegmenterStrips seg{};
+			SimulatorCPU sim{ 4, 4, con, seg };
+
+			sim.stepForward();
+			Assert::AreEqual(sim.getNumFrames(), 2);
+			for (int y = 0; y < 4; ++y) {
+				for (int x = 0; x < 4; ++x) {
+					Assert::AreEqual(sim.getCell(y, x), 0);
+				}
+			}
+
+			sim.setCell(2, 2, 1);
+			sim.setCell(2, 1, 1);
+			sim.setCell(1, 2, 1);
+			sim.stepForward();
+			Assert::AreEqual(sim.getCell(1, 1), 1);
+			Assert::AreEqual(sim.getCell(2, 1), 1);
+			Assert::AreEqual(sim.getCell(0, 0), 0);
+
+			sim.setCell(0, 1, 1);
+			sim.stepForward(1);
+			Assert::AreEqual(sim.getCell(1, 1), 0);
+			Assert::AreEqual(sim.getCell(0, 1), 1);
+
+			Assert::AreEqual(sim.getNumFrames(), 4);
+			int numFrames = 3;
+			sim.stepForward(numFrames);
+			Assert::AreEqual(sim.getNumFrames(), 7);
+		}
 	};
 }
