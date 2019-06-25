fef14f41308e337e208c73a304b6c768f2256b11
shepherd c.m. (cms1u18) <cms1u18@soton.ac.uk>
2019-06-25 11:12:53 +0100

    First version of ZonerPixels, untested so far

--- CellularAutomata/ZonerPixels.cpp
+++ CellularAutomata/ZonerPixels.cpp
@@ -13,9 +13,27 @@ ZonerPixels::~ZonerPixels()
 }
 
 bool ZonerPixels::updateDeadZones(std::vector<std::vector<std::vector<int>>> frames) {
+	for (int y = 0; y < ydim; ++y) {
+		for (int x = 0; x < xdim; ++x) {
+			cellActivities[y][x] = frames[0][y][x] != frames[1][y][x];
+		}
+	}
+
+	for (int y = 0; y < ydim; ++y) {
+		for (int x = 0; x < xdim; ++x) {
+			if (cellActivities[y][x]) {
+				for (int ypos = y - 1; ypos <= y + 1; ypos) {
+					for (int xpos = x - 1; xpos <= x + 1; ++xpos) {
+						cellActivities[y][x] = true;
+					}
+				}
+			}
+		}
+	}
+
 	return true;
 };
 
 bool ZonerPixels::isLive(int y, int x) {
-	return true;
+	return cellActivities[y][x];
 };
