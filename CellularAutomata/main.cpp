d9350952d1cac93d248888733fe4f0be6d42f96b
shepherd c.m. (cms1u18) <cms1u18@soton.ac.uk>
2019-07-02 13:59:43 +0100

    Add functionality to initialise the starting state of the simulation randomly

--- CellularAutomata/main.cpp
+++ CellularAutomata/main.cpp
@@ -49,5 +49,22 @@ int main() {
 
 	std::cout << "Sequential: " << seqTime << ", Parallelised: " << cpuTime << std::endl;
 
+	seq.clear();
+	initialiseFrame(seq,0.3);
+	
+	int liveCount = 0, deadCount = 0;
+	for (int y = 0; y < 5; ++y) {
+		for (int x = 0; x < 3; ++x) {
+			if (seq.getCell(y, x)) {
+				++liveCount;
+			}
+			else {
+				++deadCount;
+			}
+		}
+	}
+
+	std::cout << "There are " << liveCount << " live cells and " << deadCount << " dead cells!";
+
 	getchar();
 }
